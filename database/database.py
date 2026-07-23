887de04824e8736003e19a36feceeb1317a554a7
Divakar-IB <divakar.s@innoboon.com>
2026-07-23 19:23:30 +0530

    Enable SSL connection in database engine configuration

--- database/database.py
+++ database/database.py
@@ -23,7 +23,7 @@ engine = create_async_engine(
     pool_size=10,
     max_overflow=20,
     pool_recycle=1800,
-    # connect_args={"ssl": True},
+    connect_args={"ssl": True},
 )
 
 SessionLocal = sessionmaker(
